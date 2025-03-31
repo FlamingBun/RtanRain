68a9441822b60d70a05a5e337942241b45b153b2
FlamingBun <wh180911@gmail.com>
2025-03-31 16:48:35 +0900

    Update Rain.cs

--- Assets/00_Scripts/Rain.cs
+++ Assets/00_Scripts/Rain.cs
@@ -5,7 +5,7 @@ public class Rain : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Commit");
+        Debug.Log("Conflict1");
     }
 
     // Update is called once per frame
